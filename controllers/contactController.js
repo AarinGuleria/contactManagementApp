0bf9909790b04aa78ec1a9e3fc1cb40db49792ec
AarinGuleria <aaringuleria2310@gmail.com>
2025-01-17 21:38:13 +0530

    Update createContact function to return created contact; upgrade mongoose to version 8.9.5

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -20,7 +20,14 @@ const createContact =asyncHandler( async (req, res) => {
     res.status(400);
     throw new Error("All fields are mandatory !");
   }
-  res.status(201).json({ message: "Create Contact" });
+
+  const contact = await Contact.create({
+    name,
+    email,
+    phone,
+  });
+
+  res.status(201).json(contact);
 });
 
 //@desc Get contacts
