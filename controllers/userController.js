01b1a7b35681f73d8a9c18270555ccaf8f0afe5a
AarinGuleria <aaringuleria2310@gmail.com>
2025-01-18 14:07:01 +0530

    Add user registration, login, and current user routes and controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,31 @@
+const asyncHandler = require("express-async-handler");
+
 //@desc Register a new user
 //@route POST /api/users/register
 //@access public
-const getContacts = asyncHandler(async);
+const registerUser = asyncHandler(async (req, res) => {
+    res.json({
+        message: "Register the user",
+    });
+});
+
+//@desc login user
+//@route POST /api/users/login
+//@access public
+const loginUser = asyncHandler(async (req, res) => {
+    res.json({
+        message: "login user",
+    });
+});
+
+//@desc current user information
+//@route POST /api/users/current
+//@access private
+const currentUser = asyncHandler(async (req, res) => {
+    res.json({
+        message: "Current user information",
+    });
+});
+
+
+module.exports = {registerUser, loginUser, currentUser};
