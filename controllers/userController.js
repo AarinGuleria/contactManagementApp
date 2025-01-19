d9a024cd5222b36cb1dc3b90b6968d3f3b8f7bac
AarinGuleria <aaringuleria2310@gmail.com>
2025-01-19 19:51:39 +0530

    Add user registration validation and password hashing

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,25 @@
 const asyncHandler = require("express-async-handler");
+const bcrypt = require("bcrypt");
+const User = require("../models/userModel");
 
 //@desc Register a new user
 //@route POST /api/users/register
 //@access public
 const registerUser = asyncHandler(async (req, res) => {
+    const {username, email, password} = req.body;
+    if(!username || !email || !password){
+        res.status(400);
+        throw new Error("All fields are mandatory!");
+    }
+    const userAvailable = await User.findOne({email});
+    if(userAvailable){
+        res.status(400);
+        throw new Error("User already exists");
+    }
+
+    //hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+    console.log("The hashed password is: ", hashedPassword);
     res.json({
         message: "Register the user",
     });
