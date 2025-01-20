11f1d3adb76060a171f9444fe2983d66192905f9
AarinGuleria <aaringuleria2310@gmail.com>
2025-01-20 15:21:56 +0530

    Update contact routes and controller to restrict access to authenticated users

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -4,7 +4,7 @@ const Contact = require("../models/contactModel");
 
 //@desc Get all contacts
 //@route GET /api/contacts
-//@access public
+//@access private
 const getContacts = asyncHandler(async (req, res) => {
   const contacts = await Contact.find();
   res.status(200).json(contacts);
@@ -12,7 +12,7 @@ const getContacts = asyncHandler(async (req, res) => {
 
 //@desc Create New contacts
 //@route POST /api/contacts
-//@access public
+//@access private
 const createContact = asyncHandler(async (req, res) => {
   console.log("The request body is :", req.body);
   const { name, email, phone } = req.body;
@@ -32,7 +32,7 @@ const createContact = asyncHandler(async (req, res) => {
 
 //@desc Get a single contact
 //@route GET /api/contacts/:id
-//@access public
+//@access private
 const getContact = asyncHandler(async (req, res) => {
   const contact = await Contact.findById(req.params.id);
   if (!contact) {
@@ -44,7 +44,7 @@ const getContact = asyncHandler(async (req, res) => {
 
 //@desc Update a contact
 //@route PUT /api/contacts/:id
-//@access public
+//@access private
 const updateContact = asyncHandler(async (req, res) => {
   const contact = await Contact.findById(req.params.id);
   if (!contact) {
@@ -65,7 +65,7 @@ const updateContact = asyncHandler(async (req, res) => {
 
 //@desc Delete contacts
 //@route DELETE /api/contacts/:id
-//@access public
+//@access private
 const deleteContact = asyncHandler(async (req, res) => {
   console.log(`Received DELETE request for contact ID: ${req.params.id}`);
   
