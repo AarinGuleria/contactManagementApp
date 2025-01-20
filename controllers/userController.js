5b4a519b52c467545efc16b0fbfb5acfc0cd862e
AarinGuleria <aaringuleria2310@gmail.com>
2025-01-20 14:21:09 +0530

    Implement user login functionality with JWT authentication and error handling

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
 
 //@desc Register a new user
@@ -45,9 +46,30 @@ const registerUser = asyncHandler(async (req, res) => {
 //@route POST /api/users/login
 //@access public
 const loginUser = asyncHandler(async (req, res) => {
-    res.json({
-        message: "login user",
-    });
+    const {email, password} = req.body;
+    if(!email || !password){
+        res.status(400);
+        throw new Error("All fields are mandatory!");
+    }
+    const user = await User.findOne({email});
+    //compare the password with the hashed password
+    if(user && (await bcrypt.compare(password, user.password))){
+        const accessToken = jwt.sign({
+            user: {
+                username: user.username,
+                email: user.email,
+                id: user._id,
+            },
+        }, process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: "1m",
+        }
+    );
+        res.status(200).json({ accessToken });
+    }else{
+        res.status(401);
+        throw new Error("Invalid email or password");
+    }
 });
 
 //@desc current user information
